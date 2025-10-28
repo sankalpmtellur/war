d1d2e3ced49b701f54f97b24b200fe198328368a
sankalpmtellur <sankalpmtellur282006@gmail.com>
2025-10-28 10:13:37 +0530

    Added Look up page

---
+++ src/app/washerman/student-lookup/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import StudentLookup from "../StudentLookup";
+import WashermanNavbar from "../components/WashermanNavbar";
+
+export default function StudentLookupPage() {
+  return (
+    <>
+      <WashermanNavbar />
+      <StudentLookup />
+    </>
+  );
+}
